7ecc531d31f5babf9726613c0c9344c858b02a7e
Priyanshk444 <priyanshk444@git.com>
2025-01-28 21:42:54 +0530

    operations on strings

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,13 +14,30 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 //1 => true , 0 => false
 // "" => false
 // "fsdf" => true
 
 let someNumber = 22
 let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
 
+// *********Operations***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello "
+let str2 = "world"
+let str3 = str1 + str2
+console.log(str3);
