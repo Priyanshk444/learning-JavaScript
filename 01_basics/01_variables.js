76956dbadac16d89e97c1ae4410c2847fd1f4033
Priyansh Kumar <130535541+Priyanshk444@users.noreply.github.com>
2025-01-28 13:33:02 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const variableID = 12345
+let accountMail = "Priyansh@google.com"
+var accountName = "Priyansh"
+accountPassword = 4923
+let accountCity;
+
+console.table([variableID,accountMail,accountName,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict";//treat all JS code as new newer version
+
+// alert(3+3) // runs on browser not on node
+
+// null => standalone type
+// undefined => 
+
+    console.log(typeof null); // null is object
+    console.log(typeof undefined); // undefined is undefined
+    
