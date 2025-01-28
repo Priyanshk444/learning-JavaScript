4b7cf784229cbcee5d42fa1da8a827b60aa38ab7
Priyanshk444 <priyanshk444@git.com>
2025-01-28 21:29:00 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+//"33" => 33
+//"33abc" =>
+    
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 => true , 0 => false
+// "" => false
+// "fsdf" => true
+
+let someNumber = 22
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
+
