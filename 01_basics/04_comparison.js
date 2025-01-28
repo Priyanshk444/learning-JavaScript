fec8ad2ea48419d0cb4b26025aa2f24883b9db25
Priyanshk444 <priyanshk444@git.com>
2025-01-28 21:47:43 +0530

    comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+console.log(null >= 0) //in comparison null is converted to number
+console.log(null > 0)
+console.log(null == 0) // in equality check , null is not converted
+
+console.log("02" > 1)
+console.log("2" > 1)
+
+// ===
+
+console.log("2" === 1)
