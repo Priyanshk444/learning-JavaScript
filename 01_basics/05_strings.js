8a97d81563ec46af3fd653d252c11be14153f26b
Priyanshk444 <priyanshk444@git.com>
2025-01-29 18:22:19 +0530

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,27 @@
+const name = "    priyansh"
+const repoCount = 50
+
+// console.log(`my name is ${name} and my repo count is ${repoCount}`)
+
+
+const gameName = String('   Nova-5628')
+// console.log(gameName[0]);
+
+// console.log(gameName.length)
+// console.log(gameName.__proto__)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('v'))
+
+// const newString = gameName.substring(0,3)
+// console.log(newString)
+
+// const anotherString = gameName.slice(-8,3)
+// console.log(anotherString)
+
+// console.log(gameName.trim())
+
+const url = "https://x.com/priyansh%20kumar"
+
+console.log(url.replace('%20','-'))
+console.log(url.includes('%20'))
