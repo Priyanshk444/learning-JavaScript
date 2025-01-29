1eaef7baf98d23ea04b411c4904489ca69bb8dba
Priyanshk444 <priyanshk444@git.com>
2025-01-29 18:38:09 +0530

    Number and maths

---
+++ 01_basics/06_numbers_and_Maths.js
@@ -0,0 +1,20 @@
+const num = 23
+const num1 = new Number(100)
+
+// console.log(num1);
+// console.log(num1.toString.length);
+
+const otherNum = 123.434
+// console.log(otherNum.toFixed(2))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+/****************MATHS**********************/
+
+console.log(Math.random())
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
